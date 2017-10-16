ba0e89dafd8f73362593626ec89ee6ace7e9fe01
Vladyslav Papidokha <vladyslavpapidokha@gmail.com>
2017-10-16 16:34:06 +0300

    Lab 1.3

---
+++ C++ Essentials/Lab 1.3/1.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+int main()
+{
+	int v=10800;
+	int zzz=3*60;
+	int zzzz=5*60;
+	float siii=3.141526;
+	std::cout << "result: " << v << std::endl;
+// here we should print the v value but a programmer didn't have time to write any code
+// but he/she likes writing long comments
+}
